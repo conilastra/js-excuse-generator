65694f8bb726fb7937c91de359f027d46ed21e70
conilastra <cflastra@gmail.com>
2019-09-06 19:01:00 +0000

    cambios en algunos strings

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 function randomExcuse() {
-    let who = ['mi mamá ','el perro ','el vecino ','alguien demasiado importante ', 'el amor de mi vida ', 'Beyonce ', 'Jesús ', 'mi jefe ', 'un agente secreto '];
-    let what = ['me secuestró, así que no voy a poder', 'no me dio permiso para ', 'me dijo que no podía ','está en el hospital y no voy a poder ','me dijo que se iba a acabar el mundo justo cuando íbamos a '];
+    let who = ['mi mamá ','el perro ','el vecino ','alguien que no puedo delatar ', 'el amor de mi vida ', 'Beyonce ', 'Jesús ', 'mi jefe ', 'un agente secreto '];
+    let what = ['me secuestró. ', 'no me dio permiso. ', 'me prohibió ir. ','está en el hospital y tengo que ir para allá. ','me dijo que venía un meteorito y que mejor que me quedara en la casa. '];
     let closingLine = ['Perdón :(','Todo se derrumbó','Nos vemos en otra vida','Pa la próxima no fallo!','Te juro que me moría de ganas'];
-
-    let excuse = prompt('Escribe lo que quieres evitar (por ejemplo: entregar una tarea, ir a la cita Tinder)');
-
-    if (excuse == "") {
-        excuse = "ir a eso"
-    };
 
     let randomWho = Math.floor(Math.random() * (who.length));
     let randomWhat = Math.floor(Math.random() * (what.length));
     let randomClose = Math.floor(Math.random() * (closingLine.length));
 
-    let generatedExcuse = who[randomWho] + what[randomWhat] + excuse + ". " + closingLine[randomClose];
+    let generatedExcuse = who[randomWho] + what[randomWhat] + closingLine[randomClose];
 
     document.getElementById('excuse-content').innerHTML = generatedExcuse;
 }
